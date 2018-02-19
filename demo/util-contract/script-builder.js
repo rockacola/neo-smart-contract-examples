3e79a989776c534f85db4690637dc97a09244b95
Travis <travis@localhost.dev>
2018-02-20 00:49:25 +1100

    Add new util contract demo using script builder

---
+++ demo/util-contract/script-builder.js
@@ -0,0 +1,256 @@
+#!/usr/bin/env node
+
+// -- Bootstrap
+
+const Neon = require('@cityofzion/neon-js')
+const Query = Neon.rpc.Query
+const createScript = Neon.sc.createScript
+// const ContractParam = Neon.sc.ContractParam
+const fixed82num = Neon.u.fixed82num
+const hexstring2str = Neon.u.hexstring2str
+// const reverseHex = Neon.u.reverseHex
+const rpcUrls = {
+  testnetNeo2: 'http://seed2.neo.org:20332',
+  testnetNeo4: 'http://seed4.neo.org:20332',
+  mainnetNeo2: 'http://seed2.neo.org:10332',
+  mainnetNeo3: 'http://seed3.neo.org:10332', // Mainnet running old Neo version
+  privnet: 'http://127.0.0.1:30333',
+}
+const wallets = {
+  otto: {
+    address: 'AHY2eXXtkdxJ4nAJ9sWSP5n83B7nPWqBnb',
+    wif: 'KwW2MiwwVJoDCaFZw3M6iv9vtXFJtMH6Un69Sw4ZBoMYsMyBmuMG'
+  }
+}
+const contracts = {
+  UtilContract_9: '2ed7cb2fb83f5ed9c30177129da82475fbb2ac8a',
+}
+const rpcUrl = rpcUrls.testnetNeo2
+const contract = contracts.UtilContract_9
+
+// -- Chain of command
+
+async function main () {
+  console.log('== NeonJs ScriptBuilder Demo - UtilContract ==')
+
+  await versionDemo()
+  await isOwnerDemo()
+  await magicNumberDemo()
+  await magicStringDemo()
+  await neoIdDemo()
+  await addDemo()
+  await multiplyDemo()
+  await squareDemo()
+  await powerDemo()
+  await fibonacciDemo()
+
+  console.log('== END ==')
+  console.log()
+}
+
+// -- Methods
+
+async function versionDemo() {
+  console.log()
+  console.log('UtilContract.version:')
+
+  const props = {
+    scriptHash: contract,
+    operation: 'version',
+    args: []
+  }
+  const script = Neon.sc.createScript(props)
+  // console.log('script:', script)
+  const response = await Query.invokeScript(script).execute(rpcUrl)
+  // console.log('response:', response)
+  // console.log('response.result.stack:', response.result.stack)
+  const rawValue = response.result.stack[0].value
+  console.log(`rawValue: [${rawValue}]`)
+}
+
+async function isOwnerDemo() {
+  console.log()
+  console.log('UtilContract.is_owner:')
+
+  const props = {
+    scriptHash: contract,
+    operation: 'is_owner',
+    args: []
+  }
+  const script = Neon.sc.createScript(props)
+  // console.log('script:', script)
+  const response = await Query.invokeScript(script).execute(rpcUrl)
+  console.log('response:', response)
+  /**
+   * Actual response:
+    { jsonrpc: '2.0',
+      id: 1234,
+      result:
+      { script: '00c10869735f6f776e6572678aacb2fb7524a89d127701c3d95e3fb82fcbd72e',
+        state: 'FAULT, BREAK',
+        gas_consumed: '0.335',
+        stack: [] } }
+   */
+}
+
+async function magicNumberDemo() {
+  console.log()
+  console.log('UtilContract.magic_number:')
+
+  const props = {
+    scriptHash: contract,
+    operation: 'magic_number',
+    args: []
+  }
+  const script = Neon.sc.createScript(props)
+  // console.log('script:', script)
+  const response = await Query.invokeScript(script).execute(rpcUrl)
+  // console.log('response:', response)
+  // console.log('response.result.stack:', response.result.stack)
+  const rawValue = response.result.stack[0].value
+  console.log(`rawValue: [${rawValue}], fixed82num: [${fixed82num(rawValue)}]`)
+}
+
+async function magicStringDemo() {
+  console.log()
+  console.log('UtilContract.magic_string:')
+
+  const props = {
+    scriptHash: contract,
+    operation: 'magic_string',
+    args: []
+  }
+  const script = Neon.sc.createScript(props)
+  // console.log('script:', script)
+  const response = await Query.invokeScript(script).execute(rpcUrl)
+  // console.log('response:', response)
+  // console.log('response.result.stack:', response.result.stack)
+  const rawValue = response.result.stack[0].value
+  console.log(`rawValue: [${rawValue}], stringify: [${hexstring2str(rawValue)}]`)
+}
+
+async function neoIdDemo() {
+  console.log()
+  console.log('UtilContract.neo_id:')
+
+  const props = {
+    scriptHash: contract,
+    operation: 'neo_id',
+    args: []
+  }
+  const script = Neon.sc.createScript(props)
+  // console.log('script:', script)
+  const response = await Query.invokeScript(script).execute(rpcUrl)
+  // console.log('response:', response)
+  // console.log('response.result.stack:', response.result.stack)
+  const rawValue = response.result.stack[0].value
+  console.log(`rawValue: [${rawValue}]`)
+  /**
+   * Actual: 9b7cffdaa674beae0f930ebe6085af9093e5fe56b34a5c220ccdcf6efc336fc5
+   * TODO: turn hash value into wallet address
+   */
+}
+
+async function addDemo() {
+  console.log()
+  console.log('UtilContract.add:')
+
+  const props = {
+    scriptHash: contract,
+    operation: 'add',
+    args: [2, 3]
+  }
+  const script = Neon.sc.createScript(props)
+  // console.log('script:', script)
+  const response = await Query.invokeScript(script).execute(rpcUrl)
+  // console.log('response:', response)
+  // console.log('response.result.stack:', response.result.stack)
+  const rawValue = response.result.stack[0].value
+  console.log(`rawValue: [${rawValue}]`)
+}
+
+async function multiplyDemo() {
+  console.log()
+  console.log('UtilContract.multiply:')
+
+  const props = {
+    scriptHash: contract,
+    operation: 'multiply',
+    args: [7, 6]
+  }
+  const script = Neon.sc.createScript(props)
+  // console.log('script:', script)
+  const response = await Query.invokeScript(script).execute(rpcUrl)
+  // console.log('response:', response)
+  // console.log('response.result.stack:', response.result.stack)
+  const rawValue = response.result.stack[0].value
+  console.log(`rawValue: [${rawValue}]`)
+}
+
+async function squareDemo() {
+  console.log()
+  console.log('UtilContract.square:')
+
+  const props = {
+    scriptHash: contract,
+    operation: 'square',
+    args: [11]
+  }
+  const script = Neon.sc.createScript(props)
+  // console.log('script:', script)
+  const response = await Query.invokeScript(script).execute(rpcUrl)
+  // console.log('response:', response)
+  // console.log('response.result.stack:', response.result.stack)
+  const rawValue = response.result.stack[0].value
+  console.log(`rawValue: [${rawValue}]`)
+}
+
+async function powerDemo() {
+  console.log()
+  console.log('UtilContract.power:')
+
+  const props = {
+    scriptHash: contract,
+    operation: 'power',
+    args: [2, 8]
+  }
+  const script = Neon.sc.createScript(props)
+  // console.log('script:', script)
+  const response = await Query.invokeScript(script).execute(rpcUrl)
+  // console.log('response:', response)
+  // console.log('response.result.stack:', response.result.stack)
+  const rawValue = response.result.stack[0].value
+  console.log(`rawValue: [${rawValue}]`)
+}
+
+async function fibonacciDemo() {
+  console.log()
+  console.log('UtilContract.fibonacci:')
+
+  const props = {
+    scriptHash: contract,
+    operation: 'fibonacci',
+    args: [10]
+  }
+  const script = Neon.sc.createScript(props)
+  // console.log('script:', script)
+  const response = await Query.invokeScript(script).execute(rpcUrl)
+  // console.log('response:', response)
+  // console.log('response.result.stack:', response.result.stack)
+  const rawValue = response.result.stack[0].value
+  console.log(`rawValue: [${rawValue}]`)
+}
+
+// -- Execute
+
+/**
+ * Toggle to provide promise reject trace.
+ */
+const ENABLE_LOG_UNHANDLED_REJECTION = true
+if (ENABLE_LOG_UNHANDLED_REJECTION) {
+  process.on('unhandledRejection', (reason, p) => {
+    console.log('Unhandled Rejection at: Promise', p, 'reason:', reason)
+  })
+}
+
+main()
